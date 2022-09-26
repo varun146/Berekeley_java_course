a86ec23a3639bc19457491eef44fd14cf1abce6f
varun146 <varungarg63683@gmail.com>
2022-09-26 10:25:26 +0530

    deleted a text file

--- src/Map/Main.java
+++ src/Map/Main.java
@@ -8,15 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        ArrayMap<String, Integer> map = new ArrayMap<>();
-//
-//        map.put("Varun", 78);
-//        map.put("Chandan", 45);
-//        map.put("Paridhi", 90);
-//        map.put("Dad", 67);
-//        map.put("Chandan", 0000);
-//        System.out.println(map.keys());
-//        System.out.println(map.get("Varun"));
 
         Hashtable<String, Integer> ht = new Hashtable<>();
         Map<String, Integer> map = new HashMap<>();
